8aaeb5cfc9fc99e30a1000faca07c0ce81794865
Abe <abhi.rawat456@gmail.com>
2020-11-29 11:13:18 +0530

    Added ToDo component

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     class="h-screen w-screen bg-hero-light-mobile sm:bg-hero-light-desktop bg-top bg-no-repeat bg-contain sm:bg-auto"
   >
     <the-header class="mx-auto" />
+    <add-to-do />
   </section>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import AddToDo from "./components/AddToDo.vue";
 
 export default {
   components: {
     TheHeader,
+    AddToDo,
   },
 };
 </script>
---
+++ src/components/AddToDo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="relative sm:w-1/3 mx-auto">
+    <input
+      type="text"
+      placeholder="Create a new todo..."
+      class="pl-14 py-3 rounded block w-full"
+    />
+    <div
+      class="h-5 w-5 bg-white border-gray-200 border rounded-full absolute top-3 left-5"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
